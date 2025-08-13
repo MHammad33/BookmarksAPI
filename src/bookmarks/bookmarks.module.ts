30cf176bb0ee0e0bbb3a78e7736598e9e39101d3
Muhammad Hammad Afzal <hammad.afzal@werplay.com>
2025-08-13 20:39:14 +0500

    Add mongoose schema in bookmarks module

--- src/bookmarks/bookmarks.module.ts
+++ src/bookmarks/bookmarks.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Bookmark, BookmarkSchema } from './schemas/bookmark.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Bookmark.name, schema: BookmarkSchema },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+})
 export class BookmarksModule {}
